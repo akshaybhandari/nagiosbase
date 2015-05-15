c9664f16e86d84943a9891d01627b81e88191a76
akshaybhandari <akshayb.2693@gmail.com>
2015-05-15 11:53:11 +0530

    1. Added script for checking status of services 2. Added fixes for deploying nagios on RedHat Family

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -1,6 +1,6 @@
 class nagiosbase::setup {
   class { 'nagiosbase::setup::host':
-    require => [ File['/etc/nagios/nrpe.cfg'], Package['nagios-nrpe-plugin', 'nagios-nrpe-server'] ],
+    require => [ File['/etc/nagios/nrpe.cfg'], Package['nagios-nrpe-server'] ],
   } ->
   class { 'nagiosbase::setup::services': } ->
   class { 'nagiosbase::setup::files': } ->
--- manifests/setup/files.pp
+++ manifests/setup/files.pp
@@ -1,7 +1,16 @@
 class nagiosbase::setup::files {
+  case $::osfamily {
+    'redhat': {
+      $plugin_path = '/usr/lib64/nagios/plugins'
+    }
+    'debian': {
+      $plugin_path = '/usr/lib/nagios/plugins'
+    }
+    default: { fail("No such osfamily: ${::osfamily} yet defined") }
+  }
   file { "nagios_privileged_${hostname}":
     ensure => directory,
-    path   => '/usr/lib/nagios/plugins/privileged/',
+    path   => "${plugin_path}/privileged/",
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
@@ -9,10 +18,20 @@ class nagiosbase::setup::files {
   }
   file { "check_memory.sh_${hostname}":
     ensure => present,
-    path   => '/usr/lib/nagios/plugins/check_memory.sh',
+    path   => "${plugin_path}/check_memory.sh",
     source => 'puppet:///modules/nagiosbase/check_memory.sh',
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
+    require => Package['nagios-nrpe-server'],
+  }
+  file { "check_service.sh_${hostname}":
+    ensure => present,
+    path   => "${plugin_path}/check_service.sh",
+    source => 'puppet:///modules/nagiosbase/check_service.sh',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    require => Package['nagios-nrpe-server'],
   }
 }
